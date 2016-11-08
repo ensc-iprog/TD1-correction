b40a10de66f32f3298b74fe0830e3b62f4b691fb
bpesquet <bpesquet@gmail.com>
2016-11-08 15:29:33 +0100

    Exercices 8 et 9

--- Exercice08_Racines/Program.cs
+++ Exercice08_Racines/Program.cs
@@ -9,6 +9,40 @@ namespace Exercice08_Racines
     {
         static void Main(string[] args)
         {
+            Console.Write("Entrez a : ");
+            double a = int.Parse(Console.ReadLine());
+            Console.Write("Entrez b : ");
+            double b = int.Parse(Console.ReadLine());
+            Console.Write("Entrez c : ");
+            double c = int.Parse(Console.ReadLine());
+
+            int n;
+            double x1, x2;
+
+            double delta = b*b - 4*a*c;
+            if (delta >= 0)
+            {
+                if (delta == 0)
+                {
+                    x1 = x2 = -b / (2 * a);
+                    n = 1;
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    n = 2;
+                }
+
+                if (n == 1)
+                    Console.WriteLine("La solution est " + x1);
+                else
+                    Console.WriteLine("Les solutions sont " + x1 + " et " + x2);
+            }
+            else
+                Console.WriteLine("Delta négatif");
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ Exercice09_TriTableau/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Etempercice09_TriTableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = { 3, 6, 1 };
+            int temp;
+
+            if (t[0] > t[1])
+            {
+                temp = t[0];
+                t[0] = t[1];
+                t[1] = temp;
+            }
+
+            if (t[1] > t[2])
+            {
+                temp = t[1];
+                t[1] = t[2];
+                t[2] = temp;
+            }
+
+            if (t[0] > t[1])
+            {
+                temp = t[0];
+                t[0] = t[1];
+                t[1] = temp;
+            }
+
+            Console.WriteLine("{0} {1} {2}", t[0], t[1], t[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
